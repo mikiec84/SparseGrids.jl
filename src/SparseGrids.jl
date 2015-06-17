e801f3e9da89ffa9f8f07e2c3c8da5eedaac982d
Zac Nugent <zac.nugent@gmail.com>
2015-06-17 15:35:05 +0100

    sdf

--- src/SparseGrids.jl
+++ src/SparseGrids.jl
@@ -10,8 +10,6 @@ export CurtisClenshaw,
 	   grow!,
 	   shrink!
 
-
-
 include("curtisclenshaw.jl")
 include("noboundary.jl")
 include("maximum.jl")
@@ -27,7 +25,7 @@ for g in (CurtisClenshaw,Maximum,NoBoundary)
 	@eval interp(xi::Array{Float64,2},G::$g.Grid,A::Array{Float64,1}) 	= $g.interp(xi,G,A)
 	@eval getW(G::$g.Grid,A::Array{Float64}) 							= $g.getW(G,A)
 	@eval getWinv(G::$g.Grid) 											= $g.getWinv(G)
-	@eval grow!(G::$g.Grid,id,bounds::Vector{Int}=20*ones(Int,G.d))				= $g.grow!(G,id,bounds)
+	@eval grow!(G::$g.Grid,id,bounds::Vector{Int}=15*ones(Int,G.d))		= $g.grow!(G,id,bounds)
 	@eval shrink!(G::$g.Grid,id::Vector{Bool}) 							= $g.shrink!(G,id)
 	@eval show(io::IO,G::$g.Grid) 										= println(io,typeof(G)," {",G.d,",",G.q,"}[",G.n,"]")
 end
