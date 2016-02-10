665c12328fd97d51b3ed8cecc17addf6d876b3b8
Zac Nugent <zac.nugent@gmail.com>
2016-02-10 10:00:26 +0000

    d

--- src/api.jl
+++ src/api.jl
@@ -2,7 +2,7 @@ getW(G::NGrid,A::Array{Float64}) = length(G.L)>2 ? getW1(G,A) : c_getW(G,A)
 
 
 # interp{T<:GridType,BT<:BasisFunction}(xi::Array{Float64},G::NGrid{T,BT},A::Vector{Float64}) = length(G)>250 && length(G.L)>1? c_interpbig(xi,G,A) : c_interp(xi,G,A)
-interp{T<:GridType,BT<:BasisFunction}(xi::Array{Float64},G::NGrid{T,BT},A::Array{Float64}) = length(G)>250 && length(G.L)>1 ? c_interpbig(xi,G,A) : c_interp(xi,G,A)
+# interp{T<:GridType,BT<:BasisFunction}(xi::Array{Float64},G::NGrid{T,BT},A::Array{Float64}) = length(G)>250 && length(G.L)>1 ? c_interpbig(xi,G,A) : c_interp(xi,G,A)
 
 
 getW(G::NGrid{CC2Grid,LinearBF},A::Vector{Float64}) = jl_getW(G,A)
@@ -13,4 +13,4 @@ interp{T<:GridType}(xi::Array{Float64},G::NGrid{T,GaussianRadialBF},A::Array{Flo
 
 
 
-(G::NGrid)(A::Array{Float64},x::Array{Float64,2}) = interp(x,G,A)
+(G::NGrid)(A::Array{Float64},x::Array{Float64,2}) = ilength(G)>250 && length(G.L)>1 ? c_interpbig(xi,G,A) : c_interp(xi,G,A)
